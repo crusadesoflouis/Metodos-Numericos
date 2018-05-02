30f083169ef1bf5323de283f49a01e9a81615937
matiasmilla <matiasmillasson@gmail.com>
2018-05-02 16:34:16 -0300

    .

--- medidor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-seq 0.01 0.01 1 | sed 's/,/./g' | while read p; do
-	seq 1 1000 | while read dummy; do
-		./src/tp1 matrices/matriz_lado250_densidad0.2_.txt $p >> salida150
-	done
-	echo $p
-done
