ad0d4e60ae38ebf294204668b55c5f93cf5c9310
crusadesoflouis <luis.arroyo.90@gmail.com>
2018-05-08 03:25:53 -0300

    algunos casos de test

--- tp2/src/main.cpp
+++ tp2/src/main.cpp
@@ -35,15 +35,74 @@ int main(int argc, char **argv) {
     escribirArchivo(salida,solucion);*/
 
     //main para prueba de matrix
-    float arr6[] = {-3, 0.1, 1,
-                    0.1, 5, 0.6,
-                    1, 0.6, 2};
-    matrix B = crear_matriz(3, 3, arr6);
+    //K = 2.4
+    // float arr1[] = {-3, 0.1, 1,
+    //                 0.1, 5, 0.6,
+    //                 1, 0.6, 2};
+    // matrix B = crear_matriz(3, 3, arr1);
 
-    float arr7[] = {2,
-                    2,
-                    3};
-    matrix V = crear_matriz(3, 1, arr7);
+    // float arr2[] = {2,
+    //                 2,
+    //                 3};
+    // matrix V = crear_matriz(3, 1, arr2);
+    //
+    // matrix autovector(3, 1);
+    // V.normalizar();
+    //
+    // float valor = B.metodo_potencia(B, V, 5000000, autovector);
+    //
+    // autovector.mostrar();
+    // cout << "autovalor: " << valor <<'\n';
+
+
+    //K = 6.7 converge con un error de un 30%% en el autovector pero acierta en el autovalor
+    // float arr3[] = {3, 1, 2,
+    //                 1, 9, 4,
+    //                 2, 4, 6};
+    // matrix B = crear_matriz(3, 3, arr3);
+    //
+    // float arr4[] = {84,
+    //                 17,
+    //                 14};
+    // matrix V = crear_matriz(3, 1, arr4);
+    //
+    // matrix autovector(3, 1);
+    // V.normalizar();
+    //
+    // float valor = B.metodo_potencia(B, V, 5000000, autovector);
+    //
+    // autovector.mostrar();
+    // cout << "autovalor: " << valor <<'\n';
+
+    //K = 1.4 error de 2-8% en el autovector, 0 en el autovalor
+    // float arr5[] = {0, 1, 7,
+    //                 1, 9, 0.1,
+    //                 7, 0.1, 1};
+    // matrix B = crear_matriz(3, 3, arr5);
+    //
+    // float arr6[] = {84,
+    //                 17,
+    //                 14};
+    // matrix V = crear_matriz(3, 1, arr6);
+    //
+    // matrix autovector(3, 1);
+    // V.normalizar();
+    //
+    // float valor = B.metodo_potencia(B, V, 5000000, autovector);
+    //
+    // autovector.mostrar();
+    // cout << "autovalor: " << valor <<'\n';
+
+    // K = 253!!  30 a 50% en el autovector 0% en el autovalor
+    float arr5[] = {0, 1, 7,
+                    1, 0, 0.1,
+                    7, 0.1, 0};
+    matrix B = crear_matriz(3, 3, arr5);
+
+    float arr6[] = {84,
+                    17,
+                    14};
+    matrix V = crear_matriz(3, 1, arr6);
 
     matrix autovector(3, 1);
     V.normalizar();
@@ -51,7 +110,7 @@ int main(int argc, char **argv) {
     float valor = B.metodo_potencia(B, V, 5000000, autovector);
 
     autovector.mostrar();
-    cout << "autovalor:" << valor;
+    cout << "autovalor: " << valor <<'\n';
 
     return 0;
 }
--- tp2/src/matrix.cpp
+++ tp2/src/matrix.cpp
@@ -1,5 +1,5 @@
 #include <assert.h>
-#include <math.h>
+#include <cmath>
 #include "matrix.h"
 
 float EPSILON = 0.0001;
